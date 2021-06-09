aad968543378c2f9cbe86d9e8449252d383cc96a
cristi grosu <cristi.grosu.2013@mail.ru>
2021-06-09 23:35:08 +0300

    2

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: daphne -p $PORT -b 0.0.0.0 mySite.asgi:application
+worker: celery worker --app=mySite.celery.app -l DEBUG
--- mySite/asgi.py
+++ mySite/asgi.py
@@ -12,4 +12,7 @@ from .routing import ws_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mySite.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(URLRouter(ws_urlpatterns))
+})
