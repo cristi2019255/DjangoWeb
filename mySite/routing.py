0d9fde2ec5bb342b3c3e01383b36f5186be13ab2
Grosu Cristian <cristi.grosu.2013@mail.ru>
2021-06-10 10:51:56 +0300

    1.19

--- mySite/routing.py
+++ mySite/routing.py
@@ -3,10 +3,10 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter
 from channels.routing import URLRouter
 
-import mySite.mainApp.routing
+import mainApp.routing
 
 application = ProtocolTypeRouter({
     'websocket': AuthMiddlewareStack(URLRouter(
-            mySite.mainApp.routing.ws_urlpatterns
+            mainApp.routing.ws_urlpatterns
         ))
 })
