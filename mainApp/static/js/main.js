d72bdae97b86d06a349e9e6d1998fee426f5dfd0
Grosu Cristian <cristi.grosu.2013@mail.ru>
2021-06-10 16:55:53 +0300

    2.6

--- mainApp/static/js/main.js
+++ mainApp/static/js/main.js
@@ -9,8 +9,39 @@ function loadPreview(event) {
 function getBase64(file) {
       return new Promise((resolve, reject) => {
             const reader = new FileReader();
+            reader.onload = function(e) {
+                var img = new Image();
+                img.src = this.result;
+
+                setTimeout(function(){
+                    var canvas = document.createElement("canvas");
+
+                    var MAX_WIDTH = 300;
+                    var MAX_HEIGHT = 300;
+                    var width = img.width;
+                    var height = img.height;
+
+                    if (width > height) {
+                    if (width > MAX_WIDTH) {
+                    height *= MAX_WIDTH / width;
+                    width = MAX_WIDTH;
+                    }
+                    } else {
+                    if (height > MAX_HEIGHT) {
+                    width *= MAX_HEIGHT / height;
+                    height = MAX_HEIGHT;
+                    }
+                    }
+                    canvas.width = width;
+                    canvas.height = height;
+                    var ctx = canvas.getContext("2d");
+                    ctx.drawImage(img, 0, 0, width, height);
+                    var dataurl = canvas.toDataURL("image/jpeg");
+                    return dataurl.split(",")[1]
+                    },100);
+                }
             reader.readAsDataURL(file);
-            reader.onload = () => resolve(reader.result.split(",")[1]);
+            //reader.onload = () => resolve(reader.result.split(",")[1]);
             reader.onerror = error => reject(error);
       });
 }
@@ -24,6 +55,8 @@ function closeExistingSocket(socket){
 
 
 $(()=>{
+
+
 
     function hide_all() {
         $('#genetic_algorithm_params').hide()
