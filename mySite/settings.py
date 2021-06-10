36d971c30c5bfc2e3e04b7b3e25fbfc4da6573ee
Grosu Cristian <cristi.grosu.2013@mail.ru>
2021-06-10 10:48:01 +0300

    1.21

--- mySite/settings.py
+++ mySite/settings.py
@@ -129,12 +129,3 @@ CHANNEL_LAYERS = {
     }
 }
 
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.chache.RedisCache",
-        "LOCATION": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient"
-        }
-    }
-}
