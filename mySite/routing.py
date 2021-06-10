09a23e8fc2f63fb31a229b4f0ff21791d3d89d6d
Grosu Cristian <cristi.grosu.2013@mail.ru>
2021-06-10 08:13:00 +0300

    1.5

--- mySite/routing.py
+++ mySite/routing.py
@@ -9,14 +9,16 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter
 from channels.routing import URLRouter
 
-ws_urlpatterns = [
-    path('ws/some_url/', WSConsumer.as_asgi()),
-    path('ws/genetic_image_consumer/', GeneticImageConsumer.as_asgi()),
-    path('ws/pso_image_consumer/', PsoImageConsumer.as_asgi()),
-    path('ws/gan_image_consumer/', GANImageConsumer.as_asgi()),
-    path('ws/can_image_consumer/', CANImageConsumer.as_asgi()),
-]
+
 
 application = ProtocolTypeRouter({
-    'websocket': AuthMiddlewareStack(URLRouter(ws_urlpatterns))
+    'websocket': AuthMiddlewareStack(URLRouter(
+            [
+                path('ws/some_url/', WSConsumer.as_asgi()),
+                path('ws/genetic_image_consumer/', GeneticImageConsumer.as_asgi()),
+                path('ws/pso_image_consumer/', PsoImageConsumer.as_asgi()),
+                path('ws/gan_image_consumer/', GANImageConsumer.as_asgi()),
+                path('ws/can_image_consumer/', CANImageConsumer.as_asgi()),
+            ]
+        ))
 })
