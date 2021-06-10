c0bd265508ee1bebff183915b11bbe34314bd762
Grosu Cristian <cristi.grosu.2013@mail.ru>
2021-06-10 11:46:58 +0300

    2.1

--- mySite/asgi.py
+++ mySite/asgi.py
@@ -1,18 +1,7 @@
 import os
 import django
 from channels.routing import get_default_application
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter
-from channels.routing import URLRouter
-
-import mainApp.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mySite.settings')
 django.setup()
-
-application = ProtocolTypeRouter({
-    'http': get_default_application(),
-    'websocket': AuthMiddlewareStack(URLRouter(
-            mainApp.routing.ws_urlpatterns
-        ))
-})
+application = get_default_application()
